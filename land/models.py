ce6e4a72c87a2ad99fe32f3004be8dfa0eeefc6f
Lewis <lewis.murgor@student.moringaschool.com>
2024-04-01 20:29:04 +0300

    create the Land and Landmark models

--- land/models.py
+++ land/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Land(models.Model):
+    name = models.CharField(max_length=255)
+    coordinates = models.JSONField()
+
+class Landmark(models.Model):
+    name = models.CharField(max_length=255)
+    land = models.ForeignKey(Land, on_delete=models.CASCADE)
