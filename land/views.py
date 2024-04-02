fcbdfc083096bea12a71009456b6fde601830a32
Lewis <lewis.murgor@student.moringaschool.com>
2024-04-02 11:18:54 +0300

    create lands view

--- land/views.py
+++ land/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Land
+from .serializers import LandSerializer
 
 # Create your views here.
+class LandsView(generics.ListCreateAPIView):
+    queryset = Land.objects.all()
+    serializer_class = LandSerializer
