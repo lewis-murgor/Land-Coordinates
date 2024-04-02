bac2d18bfb5b714b85b6a2543b5381a043673db1
Lewis <lewis.murgor@student.moringaschool.com>
2024-04-02 13:12:29 +0300

    create more tests

--- land/tests.py
+++ land/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Land
+from django.core.exceptions import ValidationError
 
 # Create your tests here.
 class LandTest(TestCase):
@@ -20,3 +21,16 @@ class LandTest(TestCase):
         self.land.delete_land()
         land_item = Land.objects.all()
         self.assertTrue(len(land_item) == 0)
+
+    def test_invalid_land(self):
+        with self.assertRaises(ValidationError):
+            invalid_land = Land(name="", coordinates={"latitude": 0.515849, "longitude": 35.282018})
+            invalid_land.full_clean()
+
+    def test_update_land(self):
+        self.land.save_land()
+        new_name = "Different Land Name"
+        self.land.name = new_name
+        self.land.save_land()
+        updated_land = Land.objects.get(id=self.land.id)
+        self.assertEqual(updated_land.name, new_name)
