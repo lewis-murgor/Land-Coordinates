008500f041035322997fa7dd94c87696828910de
Lewis <lewis.murgor@student.moringaschool.com>
2024-04-03 13:29:02 +0300

    add authentication to views

--- land/views.py
+++ land/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics
 from .models import Land
 from .serializers import LandSerializer
 from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class LandsView(generics.ListCreateAPIView):
-    queryset = Land.objects.all()
     serializer_class = LandSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Land.objects.filter(owner=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class LandView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Land.objects.all()
     serializer_class = LandSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Land.objects.filter(owner=self.request.user)
