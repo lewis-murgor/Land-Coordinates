b6bd2117556a745076596fba669a0096a94a9661
Lewis <lewis.murgor@student.moringaschool.com>
2024-04-03 11:35:31 +0300

    create auth token url

--- land/urls.py
+++ land/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('lands', views.LandsView.as_view()),
-    path('lands/<int:pk>', views.LandView.as_view(),)
+    path('lands/<int:pk>', views.LandView.as_view()),
+    path('api-token-auth/', obtain_auth_token),
 ]
