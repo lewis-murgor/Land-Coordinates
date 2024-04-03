7f91d32c592cad84608dad1c37b0da3dbf5bbb82
Lewis <lewis.murgor@student.moringaschool.com>
2024-04-03 11:33:20 +0300

    add owner field to Land model

--- land/models.py
+++ land/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Land(models.Model):
     name = models.CharField(max_length=255)
     coordinates = models.JSONField()
     landmark = models.CharField(max_length=255, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return self.name
