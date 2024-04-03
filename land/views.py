14b310268c732608e761a4f213e34c01ad18ea99
Lewis <lewis.murgor@student.moringaschool.com>
2024-04-03 11:36:26 +0300

    add permission classes to Lands view

--- land/views.py
+++ land/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Land
 from .serializers import LandSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class LandsView(generics.ListCreateAPIView):
     queryset = Land.objects.all()
     serializer_class = LandSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class LandView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Land.objects.all()
