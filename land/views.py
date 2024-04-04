7f60c8f89882194740537bb588a8bcfc869acf98
Lewis <lewis.murgor@student.moringaschool.com>
2024-04-04 12:00:37 +0300

    add permissions to landview

--- land/views.py
+++ land/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import Land
 from .serializers import LandSerializer
 from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 class LandsView(generics.ListCreateAPIView):
@@ -10,14 +11,12 @@ class LandsView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Land.objects.filter(owner=self.request.user)
+        return Land.objects.all()
     
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 class LandView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Land.objects.all()
     serializer_class = LandSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        return Land.objects.filter(owner=self.request.user)
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
